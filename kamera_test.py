a4a69a4caf11dcc2f8d89740e3cc093374478cb4
Yunus Emre Tom <yunusemretom@gmail.com>
2024-09-16 22:48:00 +0300

    Dosya adlandırma ve içerik değiştirm

--- kamera_test.py
+++ kamera_test.py
@@ -6,14 +6,14 @@ Kamera test kodu.
 
 import cv2
 
-cap = cv2.VideoCapture(0) # Kamera idsi ne ise o konulmalı. Örneğin 0 yerine 1,2,3 gibi 
+cap = cv2.VideoCapture(0) # Kamera id'si ne ise o konulmalı. Örneğin 0 yerine 1,2,3 gibi, Bazen de direk port verebilirsiniz. Örn: COM6, ACM0 gibi... 
 
 while True:
     _,img = cap.read()
 
+
     cv2.imshow("img",img)
-    
-    if cv2.waitKey(1) ==27: # ESC ile çıkma kodu 
+    if cv2.waitKey(1) ==27: # ESC çıkma kodu
         break
 
 cv2.destroyAllWindows() 
